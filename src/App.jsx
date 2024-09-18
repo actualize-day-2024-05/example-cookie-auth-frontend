2dfbcedd297c87b6d64f3393828c7c679b92c23b
Peter Jang <peter.x.jang@gmail.com>
2024-09-18 13:28:03 -0500

    Add authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import axios from "axios";
 import { Header } from "./Header";
 import { PhotosPage } from "./PhotosPage";
+import { SignupPage } from "./SignupPage";
+import { LoginPage } from "./LoginPage";
 import { Footer } from "./Footer";
+
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
     <div>
       <Header />
+      <SignupPage />
+      <LoginPage />
       <PhotosPage />
       <Footer />
     </div>
